d344048f6a9efe12798496c149ae7baebb4286bb
Ore <efyoa12@nottingham.ac.uk>
2026-02-12 06:03:03 +0000

    Exercise 3: multiple source files compiled and linked

---
+++ worksheet3/adder.h
@@ -0,0 +1,10 @@
+// begin = adder.h ============
+// Include guard prevents multiple includes
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+// prototype for our function
+int add(int a, int b);
+
+#endif
+// end = adder.h =============
