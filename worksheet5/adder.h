128ac3c9a4bfecab5e1da7f5a8dd06f9ddd4cce4
Ore <efyoa12@nottingham.ac.uk>
2026-02-13 11:27:57 +0000

    Worksheet 5: Added Doxygen config and documentation

---
+++ worksheet5/adder.h
@@ -0,0 +1,41 @@
+// begin = adder.h 
+
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/** @file adder.h
+ * Declarations for the maths library.
+ */
+
+/** Add two integers.
+ * @param a First integer
+ * @param b Second integer
+ * @return a + b
+ */
+
+
+// Define MATHSLIB_API depending on platform and whether we are building or using the DLL
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef maths_STATIC
+    // Building/using as a static library
+    #define MATHSLIB_API
+  #else
+    #ifdef maths_EXPORTS
+      // Building the DLL
+      #define MATHSLIB_API __declspec(dllexport)
+    #else
+      // Using the DLL
+      #define MATHSLIB_API __declspec(dllimport)
+    #endif
+  #endif
+#else
+  // Non-Windows platforms
+  #define MATHSLIB_API
+#endif
+
+// function prototype including API keyword
+MATHSLIB_API int add(int a, int b);
+
+#endif
+
+// end = adder.h 
